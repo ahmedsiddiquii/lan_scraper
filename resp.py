7defdcc968ca238449360e539d183946b64ebd23
M.sharjeel/Orion <75837471+sharjeel-rbg@users.noreply.github.com>
2023-08-04 11:19:16 -0700

    Add files via upload

--- resp.py
+++ resp.py
@@ -8,17 +8,9 @@ def send_json_post_request():
         'type': 'restaurant',
         'number_of_data': 10
     }
-    session = requests.Session()
 
-     # Extract the CSRF token from the cookies
-
-    # Step 2: Make a POST request, including the CSRF token in the headers
-    headers = {
-        'Referer': url,  # Including the Referer header is often required
-    }
-
-    response = session.post(url, json=data,headers=headers)
-    # print(response.text)
+    # Step 1: Make a POST request to save data
+    response = requests.post(url, json=data)
 
     if response.status_code == 200:
         print("POST request successful!")
@@ -26,6 +18,23 @@ def send_json_post_request():
     else:
         print(f"POST request failed with status code {response.status_code}.")
 
+    # Step 2: Make a DELETE request to remove data with the same latitude, longitude, or type
+    delete_url = 'http://127.0.0.1:8000/delete_data/'
+    delete_data = {
+        'longitude': '-123.1207',
+        'latitude': '49.2827',
+        'type': 'restaurant'
+    }
+
+    delete_response = requests.delete(delete_url, json=delete_data)
+
+    if delete_response.status_code == 200:
+        print("DELETE request successful!")
+        print("Response JSON:", delete_response.json())
+    elif delete_response.status_code == 404:
+        print("No matching data found for deletion.")
+    else:
+        print(f"DELETE request failed with status code {delete_response.status_code}.")
+
 if __name__ == "__main__":
     send_json_post_request()
-
